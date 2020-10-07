e6164d59b61ae4fde0f514218c3e03f315aa71f5
Leonid.Boyko <leonid.boyko@computerstein.net>
2020-10-07 13:33:16 +0300

    first views

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tweet(models.Model):
+    content = models.TextField()
+
+    class Meta:
+        verbose_name = 'Tweet'
+        verbose_name_plural = 'Tweets'
+
+    def __str__(self):
+        return str(self.id)
+
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import Http404, HttpResponse, render
 
-# Create your views here.
+from .models import Tweet
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse('<h1>Hello word!!</h1>')
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+        data
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status)
